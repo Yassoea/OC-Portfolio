3a9de4ad55cc5cc7f29d4e9a0bf72fce9bf1c90e
Yassoea <elakhalyassine10@gmail.com>
2024-12-18 19:32:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/*First Section*/
+const textElement = document.getElementById('text');
+const cursorElement = document.querySelector('.cursor');
+
+const textArray = [
+  "Je suis Yassin El Akhal", 
+  "Développeur Web & Application",
+];
+const typingSpeed = 100; 
+const delayBetweenLines = 800; 
+
+let lineIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+  if (lineIndex < textArray.length) {
+    const currentLine = textArray[lineIndex];
+    if (charIndex < currentLine.length) {
+      textElement.innerHTML += currentLine[charIndex]; 
+      charIndex++;
+      setTimeout(typeText, typingSpeed);
+    } else {
+      if (lineIndex < textArray.length - 1) {
+        textElement.innerHTML += "<br>"; 
+      }
+      charIndex = 0;
+      lineIndex++;
+      setTimeout(typeText, delayBetweenLines);
+    }
+  } else {
+    
+    setTimeout(() => {
+      textElement.innerHTML = ""; 
+      lineIndex = 0;
+      charIndex = 0;
+      typeText(); 
+    }, delayBetweenLines);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeText();
+});
+/*First Section*/
